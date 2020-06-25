0b414427a3e36433cc02e9f30b9b6dff6ccda075
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-06-25 15:56:03 +0200

    Create 2nd solution

--- 4.2_zad1.py
+++ 4.2_zad1.py
@@ -6,5 +6,17 @@ def palindrom_check(word):
         word_backward.insert(-len(word),letter)
     palindrom_result= bool(word_normal==word_backward)
     return palindrom_result
+
+
 print(palindrom_check('kot'))
 print(palindrom_check('kajak'))
+
+def palindrom_sprawdzenie(slowo):
+    slowo_normal=[]
+    for litera in slowo:
+        slowo_normal.append(litera)
+    palindrom_result2=bool(slowo_normal==slowo_normal[::-1])
+    return palindrom_result2
+
+
+print(palindrom_sprawdzenie('kajak'))
